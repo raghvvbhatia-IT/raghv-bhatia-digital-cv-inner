7984adccb1d5d92f97aa39a674b35bf9f6b03579
raghvbht17 <149209863+raghvbht17@users.noreply.github.com>
2026-03-16 22:37:10 +1300

    Add Vercel analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { inject } from '@vercel/analytics';
 import './index.css';
 import App from './App';
+
+inject();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<App />);
